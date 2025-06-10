523275eeff6a0dd744f7a3c1d2fdbd315e2050a4
ILLIA <bobicp01@gmail.com>
2025-06-10 09:52:05 +0200

    Updates

---
+++ precache-manifest.1536c6f44fa8dc4be9af80a032664b55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e5047e1f00a97a49473f23d9811952",
+    "url": "/react-org-chart/index.html"
+  },
+  {
+    "revision": "47a435ea0bffdba3b03e",
+    "url": "/react-org-chart/static/css/main.18784a68.chunk.css"
+  },
+  {
+    "revision": "3be80ad3f2262f926632",
+    "url": "/react-org-chart/static/js/2.748d1f8b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-org-chart/static/js/2.748d1f8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a435ea0bffdba3b03e",
+    "url": "/react-org-chart/static/js/main.65b928e5.chunk.js"
+  },
+  {
+    "revision": "bb41dfa41bd2945e9ca534fcd6227399",
+    "url": "/react-org-chart/static/js/main.65b928e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a897ade332a5b6f43d11",
+    "url": "/react-org-chart/static/js/runtime-main.d9ba5a7a.js"
+  },
+  {
+    "revision": "112b90dac27346a0a6bba646dc977dcb",
+    "url": "/react-org-chart/static/media/avatar-personnel.112b90da.svg"
+  }
+]);
